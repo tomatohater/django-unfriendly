4175fd4d285015eae7b0b51e719fd768a1a75468
Drew Engelson <drew@engelson.net>
2011-08-11 22:04:10 -0400

    Commented out buggy checksum logic.

--- unfriendly/utils.py
+++ unfriendly/utils.py
@@ -24,8 +24,8 @@ def encrypt(plaintext, secret, lazy=True, checksum=True):
     secret = _lazysecret(secret) if lazy else secret
     encobj = AES.new(secret, AES.MODE_CFB)
 
-    if checksum:
-        plaintext += struct.pack("i", zlib.crc32(plaintext))
+    #if checksum:
+    #    plaintext += struct.pack("i", zlib.crc32(plaintext))
 
     return base64.urlsafe_b64encode(encobj.encrypt(plaintext)).replace('=', '')
 
@@ -43,9 +43,9 @@ def decrypt(ciphertext, secret, lazy=True, checksum=True):
     plaintext = encobj.decrypt(base64.urlsafe_b64decode(
         ciphertext + ('=' * (len(ciphertext) % 4))))
 
-    if checksum:
-        crc, plaintext = (plaintext[-4:], plaintext[:-4])
-        if not crc == struct.pack("i", zlib.crc32(plaintext)):
-            raise CheckSumError("checksum mismatch")
+    #if checksum:
+    #    crc, plaintext = (plaintext[-4:], plaintext[:-4])
+    #    if not crc == struct.pack("i", zlib.crc32(plaintext)):
+    #        raise CheckSumError("checksum mismatch")
 
     return plaintext
