014a42887470abc7a72860a9c509a1472d57be74
Kyle Amos <kyle.amos@vertical-knowledge.com>
2016-07-11 15:06:59 -0400

    Attempting to fix URLs for Django 1.10

--- unfriendly/urls.py
+++ unfriendly/urls.py
@@ -1,20 +1,40 @@
 # -*- coding: utf-8 -*-
 """Url handlers."""
 
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    # For Django versions older than 1.4 (removed in 1.6)
-    from django.conf.urls.defaults import patterns, url
+import django
+from distutils.version import StrictVersion
 
+DJANGO_VERSION = StrictVersion(django.get_version)
 
-urlpatterns = patterns(
-    'unfriendly.views',
+if DJANGO_VERSION >= StrictVersion('1.9'):
 
-    # Mostly unfriendly URL (but with SEO juice).
-    url(r'^(?P<juice>.+)/(?P<key>.+)/$', 'deobfuscate',
-        name='unfriendly-deobfuscate'),
+    from django.conf.urls import url
+    from unfriendly.views import deobfuscate
 
-    # Extremely unfriendly URL (no SEO juice).
-    url(r'^(?P<key>.+)/$', 'deobfuscate', name='unfriendly-deobfuscate'),
-)
+    urlpatterns = [
+        # Mostly unfriendly URL (but with SEO juice).
+        url(r'^(?P<juice>.+)/(?P<key>.+)/$', deobfuscate,
+            name='unfriendly-deobfuscate'),
+
+        # Extremely unfriendly URL (no SEO juice).
+        url(r'^(?P<key>.+)/$', deobfuscate, name='unfriendly-deobfuscate')
+    ]
+
+else:
+
+    if DJANGO_VERSION >= StrictVersion('1.5'):
+        from django.conf.urls import patterns, url
+    else:
+        from django.conf.urls.defaults import patterns, url
+
+    urlpatterns = patterns(
+        'unfriendly.views',
+
+        # Mostly unfriendly URL (but with SEO juice).
+        url(r'^(?P<juice>.+)/(?P<key>.+)/$', 'deobfuscate',
+            name='unfriendly-deobfuscate'),
+
+        # Extremely unfriendly URL (no SEO juice).
+        url(r'^(?P<key>.+)/$', 'deobfuscate', name='unfriendly-deobfuscate'),
+    )
+
